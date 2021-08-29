c9f800cc3b6416ca699fea30bc0220dfb8c0a474
kucukkaa <50020610+kucukkaa@users.noreply.github.com>
2021-08-29 16:45:26 +0300

    29.08.2021

---
+++ DevFramework.DataAccess.Tests/NHibernateTests/NHibernateTest.cs
@@ -0,0 +1,32 @@
+﻿using DevFramework.Northwind.DataAccess.Concrete.NHibernate;
+using DevFramework.Northwind.DataAccess.Concrete.NHibernate.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DevFramework.DataAccess.Tests.NHibernateTests
+{
+    [TestClass]
+    public class NHibernateTest
+    {
+        [TestMethod]
+        public void Get_all_returns_all_products()
+        {
+            NhProductDal productDal = new NhProductDal(new SqlServerHelper());
+
+            var result = productDal.GetList();
+
+            Assert.AreEqual(80, result.Count);
+        }
+
+        [TestMethod]
+        public void Get_all_with_parameter_returns_filtered_products()
+        {
+            NhProductDal productDal = new NhProductDal(new SqlServerHelper());
+
+            var result = productDal.GetList(p => p.ProductName.Contains("ab"));
+
+            Assert.AreEqual(5, result.Count);
+        }
+
+    }
+}
--- DevFramework.Northwind.Entities/Properties/AssemblyInfo.cs
+++ DevFramework.Northwind.Entities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClDevFramework.Northwind.Entities")]
+[assembly: AssemblyTitle("DevFramework.Northwind.Entities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClDevFramework.Northwind.Entities")]
+[assembly: AssemblyProduct("DevFramework.Northwind.Entities")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("705b47a8-ecb3-478f-8f6f-b7be7d1d2343")]
+[assembly: Guid("70636a14-054c-435e-8511-7808b3754ee3")]
 
 // Version information for an assembly consists of the following four values:
 //
