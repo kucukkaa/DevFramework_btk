9296671175360ceb60b3eac656988bb874efed31
kucukkaa <50020610+kucukkaa@users.noreply.github.com>
2021-09-30 14:17:36 +0300

    30.09.2021

---
+++ DevFramework.Northwind.MvcWebUI/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using DevFramework.Core.CrossCuttingConcerns.Security.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevFramework.Northwind.MvcWebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public string Login()
+        {
+            AuthenticationHelper.CreateAuthCookie(new Guid(), "alierkkucuk", "alierkkucuk@gmail.com", DateTime.Now.AddDays(15), new[] { "Admin" }, false, "alierk", "küçük");
+            return "User is authenticated!";
+        }
+    }
+}
